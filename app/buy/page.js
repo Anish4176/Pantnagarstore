fca395d342d44c19148ef77d0b9268b8b8b2b569
Anish <anishsingh41764176@gmail.com>
2024-04-21 00:37:14 +0530

    added magnifying glass in search input

--- app/buy/page.js
+++ app/buy/page.js
@@ -45,7 +45,7 @@ const Buy = () => {
     <div className="min-h-[100vh]">
       <div className="mx-4">
         <label
-          className="mx-auto border-2 mt-10 relative bg-white min-w-sm max-w-2xl flex flex-col md:flex-row items-center justify-center  py-2 px-2  rounded-2xl gap-2  focus-within:border-gray-300"
+          className="mx-auto  border-2 mt-10 relative bg-white min-w-sm max-w-2xl flex flex-col md:flex-row items-center justify-center  py-2 px-2  rounded-2xl gap-2  focus-within:border-gray-300"
           htmlFor="search-bar"
         >
           <input
@@ -53,8 +53,24 @@ const Buy = () => {
             value={searchText}
             onChange={handleSearch}
             placeholder="Search Any Product or Category"
-            className="px-6 py-2 w-full rounded-md flex-1 outline-none bg-white"
+            className="md:px-10 px-8 py-2  w-full rounded-md flex-1 outline-none bg-white"
           />
+          <div class="absolute left-0 inset-y-0 flex items-center ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 ml-3 text-gray-400 hover:text-gray-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
         </label>
       </div>
 
@@ -105,9 +121,12 @@ const Buy = () => {
               </div>
             ))
           ) : (
-            Array.from({length: 9}, (_, i) => (
-              <div className="w-1/2 md:w-1/3 lg:w-1/4  px-1 md:px-2 flex items-center justify-center" key={i}>
-              <Loader/>
+            Array.from({ length: 9 }, (_, i) => (
+              <div
+                className="w-1/2 md:w-1/3 lg:w-1/4  px-1 md:px-2 flex items-center justify-center"
+                key={i}
+              >
+                <Loader />
               </div>
             ))
           )}
